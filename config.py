ea43d4016fd773197b90f6d4237d13c8ba07d9af
proyectosigap <zmamanir@unmsm.edu.pe>
2022-08-09 21:43:41 -0500

    version 1.0.0

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 load_dotenv()
 
-user = os.environ['POSTGRESQL_USER']
-pwd = os.environ['POSTGRESQL_PASSWORD']
-host = os.environ['POSTGRESQL_HOST']
-database = os.environ['POSTGRESQL_DATABASE']
-server = os.environ['POSTGRESQL_SERVER']
+user = os.environ['USER']
+pwd = os.environ['PASSWORD']
+host = os.environ['HOST']
+database = os.environ['DATABASE']
+server = os.environ['SERVER']
 
 DATABASE_CONNECTION= f'{server}://{user}:{pwd}@{host}/{database}'
