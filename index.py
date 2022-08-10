91d195186e20a292b9cc7e73915b82d5f88864d5
proyectosigap <zmamanir@unmsm.edu.pe>
2022-08-09 21:36:31 -0500

    version 1.0.0

--- index.py
+++ index.py
@@ -5,4 +5,5 @@ with app.app_context():
     db.create_all()
 
 if __name__== "__main__":    
-    app.run(host='0.0.0.0', debug=False, port=5000)
+    #app.run(host='0.0.0.0', debug=False, port=5000)
+    app.run(debug=True)
